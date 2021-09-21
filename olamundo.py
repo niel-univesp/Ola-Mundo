84b9f3d975b0c1c195ad29ac65933adcf1ba6aa7
niel-univesp <2004073@aluno.univesp.br>
2021-09-20 21:15:08 -0300

    Hello GitHub

--- olamundo.py
+++ olamundo.py
@@ -4,10 +4,12 @@ mensagem2 = 'Olá mundo'
 mensagem001 = 'Bem-Vindos ao arquivo da 2020115'
 mensagem3 = "Bem-Vindos ao nosso mundo!"
 mensagem02 = " hoje é dia 20/05/2021"
+msg01 = "Hello Github!"
 print(mensagem, mensagem02)
 
 
 # print("Hello World!")
 
 print(mensagem, mensagem001)
-print(mensagem3) 
+print(mensagem3)
+print(msg01)
